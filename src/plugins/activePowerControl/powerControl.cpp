01def965b9b93e113b3ce7f06e8d08a9c9151242
ASparwasser <asp@akkuvision.de>
2023-03-11 23:35:21 +0100

    response  from shelly works

--- src/plugins/activePowerControl/powerControl.cpp
+++ src/plugins/activePowerControl/powerControl.cpp
@@ -8,6 +8,7 @@
 #include <string>
 
 AsyncClient * aClient = NULL;
+bool connectionOK = false;
 
 void powerControl::setup(settings_t* config)
 {
@@ -29,35 +30,50 @@ void powerControl::tickPowerControlLoop_1s(void)
         DBGPRINT(F("times"));
 
         runAsyncClient();
+        if(true == connectionOK)
+        {
+            //send the request
+            //aClient->write("GET / HTTP/1.1\r\nHost: http://192.168.178.158/status\r\n\r\n");
+            aClient->write("GET /status HTTP/1.1\r\n");
+            aClient->write("Host: 192.168.178.158\r\n");
+            aClient->write("\r\n");
+        }
+
     }
 }
 
 
 
 void powerControl::runAsyncClient(){
-    DBGPRINT(F("runAsyncClient called \n"));
+    DBGPRINTLN(F("runAsyncClient called \n"));
   if(aClient)//client already exists
     return;
 
-  DBGPRINT(F("runAsyncClient stage 1 \n"));
+  DBGPRINTLN(F("runAsyncClient stage 1 \n"));
   aClient = new AsyncClient();
   if(!aClient)//could not allocate client
     return;
 
-  DBGPRINT(F("runAsyncClient stage 2 \n"));
+  DBGPRINTLN(F("runAsyncClient stage 2 \n"));
 
-  aClient->onError([](void * arg, AsyncClient * client, int error){
-    DBGPRINT(F("Connect Error"));
+  aClient->onError([](void * arg, AsyncClient * client, err_t error){
+    connectionOK = false;
+    DBGPRINT(F("Connect Error: "));
+    DBGPRINT(String(error));
+    DBGPRINT(F("\n"));
+
     aClient = NULL;
     delete client;
   }, NULL);
 
   aClient->onConnect([](void * arg, AsyncClient * client){
-    DBGPRINT(F("Connected"));
+    connectionOK = true;
+    DBGPRINTLN(F("Connected"));
     aClient->onError(NULL, NULL);
 
     client->onDisconnect([](void * arg, AsyncClient * c){
-      DBGPRINT(F("Disconnected"));
+      DBGPRINTLN(F("Disconnected"));
+      connectionOK = false;
       aClient = NULL;
       delete c;
     }, NULL);
@@ -67,22 +83,25 @@ void powerControl::runAsyncClient(){
       DBGPRINT(String(len));
       DBGPRINT(F("Data: \n"));
 
+
       uint8_t * d = (uint8_t*)data;
       char ch;
+
       for(size_t i=0; i<len;i++)
       {
         ch = (char)d[i];
         DBGPRINT(String(ch));
       }
 
+      connectionOK = true;
+      //c->close();
     }, NULL);
 
-    //send the request
-    client->write("GET / HTTP/1.0\r\nHost: 192.168.178.158/status\r\n\r\n");
   }, NULL);
 
-  if(!aClient->connect("192.168.178.158/status", 80)){
-    DBGPRINT(F("Connect Fail"));
+  if(!aClient->connect("192.168.178.158", 80))
+  {
+    DBGPRINTLN(F("Connect Fail"));
     AsyncClient * client = aClient;
     aClient = NULL;
     delete client;
